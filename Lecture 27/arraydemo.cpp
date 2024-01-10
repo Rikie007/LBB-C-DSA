dcab0547954804714237ca14d70e993e7d7814ed
Rikie007 <156202573+Rikie007@users.noreply.github.com>
2024-01-11 00:46:29 +0530

    Add files via upload

---
+++ Lecture 27/arraydemo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100] = {0};
+    cout<< &arr << endl;
+    cout << arr << endl;
+    cout << &arr[0] << endl;
+    cout << arr[0] << endl;
+    return 0;
+    
+}
